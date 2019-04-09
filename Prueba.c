b8d2d49037df2b06786ad44b148e1bdba54495c1
sergioLlorens <sergio.llorens@edu.uah.es>
2019-04-09 20:25:14 +0200

    Prueba

--- Prueba.c
+++ Prueba.c
@@ -10,7 +10,8 @@ int main(){
     printf("El siguiente numero entero es: %i \n", num1);
     printf("El siguiente numero Long es: %lu \n", num2);
     printf("El siguiente numero Float es: %f \n", numFloat);
-    printf("El siguiente numero Double es: %lf \n", numDouble);      
+    printf("El siguiente numero Double es: %lf \n", numDouble); 
+         
 
     return 0;
 }
